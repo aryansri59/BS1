c12087cb5db01eb33fcdd3f51bdb8834eb43bc12
Aryan Raj <aryanraj.csdev@gmail.com>
2025-06-02 19:20:25 +0530

    Initial Commit

---
+++ src/BinarySearch/BS1.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+public class BS1 {
+
+    public static void main(String []args) {
+
+        int []arr = {11, 22, 33, 44, 55};
+        int start = 0, end = arr.length-1;
+        int find_key = 22;
+        int mid = (start+end) / 2;
+
+        while(end >= start) {
+
+            if(arr[mid] == find_key) {
+                System.out.println(mid);       // Print index value
+                break;
+            }
+            else if(arr[mid] < find_key) {
+                start = mid + 1;
+                mid = (start+end) / 2;
+            }
+            else if(arr[mid] > find_key) {
+                end = mid - 1;
+                mid = (start+end) / 2;
+            }
+        }
+
+    }
+
+}
