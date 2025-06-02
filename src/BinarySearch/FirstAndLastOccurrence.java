2475cd3f66e0bd2fe0c3e77b64eb0b1df73a38b7
Aryan Raj <aryanraj.csdev@gmail.com>
2025-06-02 19:41:59 +0530

    Add Initial file

---
+++ src/BinarySearch/FirstAndLastOccurrence.java
@@ -0,0 +1,58 @@
+package BinarySearch;
+
+public class FirstAndLastOccurrence {
+
+    public static int firstOccurrence(int[] arr, int key) {
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = -1;
+
+        int mid = start + (end - start) / 2;
+
+        while(start <= end) {
+            if(arr[mid] == key) {
+                ans = mid;
+                end = mid - 1;
+            }
+            else if(arr[mid] < key) {
+                start = mid + 1;
+            }
+            else if(arr[mid] > key) {
+                end = mid - 1;
+            }
+            mid = start + (end - start) / 2;
+        }
+        return ans;
+    }
+
+    public static int lastOccurrence(int[] arr, int key) {
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = -1;
+
+        int mid = start + (end - start) / 2;
+
+        while(start <= end) {
+            if(arr[mid] == key) {
+                ans = mid;
+                start = mid + 1;
+            }
+            else if(arr[mid] < key) {
+                start = mid + 1;
+            }
+            else if(arr[mid] > key) {
+                end = mid - 1;
+            }
+            mid = start + (end - start) / 2;
+        }
+        return ans;
+    }
+
+    public static void main(String []args) {
+        int []arr = {1, 2, 4, 5, 5, 6};
+        int storeFirst = firstOccurrence(arr, 9);
+        int storeLast = lastOccurrence(arr, 8);
+        System.out.println(storeFirst+ " " + storeLast);
+    }
+
+}
